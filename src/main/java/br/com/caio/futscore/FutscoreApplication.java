797a12162506b3abf4cbe35b7a7f73d00b6a70f8
caiorodri <caiodossantosrodrigues@gmail.com>
2024-09-12 14:33:44 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/caio/futscore/FutscoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.caio.futscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caio/futscore/FutscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caio.futscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
